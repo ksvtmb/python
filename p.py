9ab08fe2010db6931d4e9024719fe295083ba056
Kascheev S.V <info@ksvtmb.ru>
2019-06-07 19:43:02 +0300

    Шедрый посетиль готово

--- p.py
+++ p.py
@@ -4,3 +4,7 @@ name=input("Hello. Whats you name?")
 age=input("So, how old are you?")
 age=int(age)
 weight=int(input("how much do you weigh?"))
+
+called=name*5
+print("child says:")
+print(called)
