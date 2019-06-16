656ff2e1c6b976893a82cb3be9fc7c9df91ccce4
Kascheev S.V <info@ksvtmb.ru>
2019-06-16 13:55:21 +0300

    игра с кортежами

--- kortej1.py
+++ kortej1.py
@@ -9,8 +9,19 @@ inventory=("меч","кольчуга","коктейль молотова","ко
 
 # выводим наш eq
 
-print("наша экипа: ",inventory)
+print("наша экипа: ",inventory, "и в ней: ",len(inventory),"предметов")
+
+if "коктейль молотова" in inventory:
+    print("да вы батюшка! экстремист")
+    input("за тобой уже выехали!")
 
 print("выведем по порядку")
 for item in inventory:
     print (item)
+
+print("демонстрируем не сильную штуку:", inventory[3])
+print("демонстрируем Oldschool:", inventory[:2])
+
+added=("бутылка сэма", "визитка яроша")
+inventory+=added
+print(inventory)
