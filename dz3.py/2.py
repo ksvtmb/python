bf473f5ed988ee96a04d9127e118c8372d0ae477
Kascheev S.V <info@ksvtmb.ru>
2019-06-17 18:55:04 +0300

    мелкие доработки

--- dz3.py/2.py
+++ dz3.py/2.py
@@ -1,17 +1,11 @@
 #выводим слово наоборот
 
-string=input("введи слово, которое стоит выверуть!: " )
-print("слово:",string)
-
+string=input("введи слово, которое стоит выверуть: " )
 change_string=""
-
-dlina=-len(string)
-
 n=-1
-while n>=dlina:
-    # print(n)
+
+while n>=-len(string):
     change_string+=string[n]
     n-=1
-
 
 print("слово наоборот: ",change_string)
