535711076316990fecf4c4a450e949cab248cd9b
Kascheev S.V <info@ksvtmb.ru>
2019-06-20 23:49:09 +0300

    adding count (len) item in a list

--- spis1.py
+++ spis1.py
@@ -4,3 +4,4 @@ inventory=["sword", "helmet", "shield","armor vest", "heal potion"]
 print("my inventory: ")
 for item in inventory:
     print(item)
+print("in you equ is a:", len(inventory), "items")
