52a3d0451ffaee097cc08bf7e7470509d9b41dc6
Kascheev S.V <info@ksvtmb.ru>
2019-06-20 23:50:52 +0300

    check heal potion

--- spis1.py
+++ spis1.py
@@ -5,3 +5,8 @@ print("my inventory: ")
 for item in inventory:
     print(item)
 print("in you equ is a:", len(inventory), "items")
+
+# check heal potion in eq
+
+if "heal potion" in inventory:
+    print("you will live")
